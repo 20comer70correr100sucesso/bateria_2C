c38683b3c30ad5cd0a957c613852844d70391bd6
20comer70correr100sucesso <130760108+20comer70correr100sucesso@users.noreply.github.com>
2023-11-17 08:32:55 -0300

    b

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function Pom (){
     document.querySelector('#som_tecla_pom').play();
     }
     document.querySelector('.tecla_pom') .onclick = Pom;
+    
     function Clap (){
         document.querySelector('#som_tecla_Clap').play();
         }
