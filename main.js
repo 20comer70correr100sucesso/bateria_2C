83fa35d4ac38bc768a201a10fe377da48d66ac41
20comer70correr100sucesso <130760108+20comer70correr100sucesso@users.noreply.github.com>
2023-11-17 08:27:55 -0300

    y

--- main.js
+++ main.js
@@ -2,4 +2,44 @@ function Pom (){
     document.querySelector('#som_tecla_pom').play();
     }
     document.querySelector('.tecla_pom') .onclick = Pom;
-    
+    function Clap (){
+        document.querySelector('#som_tecla_Clap').play();
+        }
+        document.querySelector('.tecla_Clap') .onclick = Clap;
+   
+        function Tim (){
+            document.querySelector('#som_tecla_Tim').play();
+            }
+            document.querySelector('.tecla_Tim') .onclick = Tim;
+   
+            function Puff (){
+                document.querySelector('#som_tecla_Puff').play();
+                }
+                document.querySelector('.tecla_Puff') .onclick = Puff;
+   
+                function Splashm (){
+                    document.querySelector('#som_tecla_Splash').play();
+                    }
+                    document.querySelector('.tecla_Splash') .onclick = Splash;
+   
+                    function Toim (){
+                        document.querySelector('#som_tecla_Toim').play();
+                        }
+                        document.querySelector('.tecla_Toim') .onclick = Toim;
+   
+                        function Psh (){
+                            document.querySelector('#som_tecla_Psh').play();
+                            }
+                            document.querySelector('.tecla_Psh') .onclick = Psh;
+   
+                            function Tic (){
+                                document.querySelector('#som_tecla_Tic').play();
+                                }
+                                document.querySelector('.tecla_Tic') .onclick = Tic;
+   
+                                function Tom (){
+                                    document.querySelector('#som_tecla_Tom').play();
+                                    }
+                                    document.querySelector('.tecla_Tom') .onclick = Tom;
+                                   
+   
